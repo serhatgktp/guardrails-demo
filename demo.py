045a9211760f980e9e71faa5d81ae47542d620cb
Efkan S. Goktepe <76130958+serhatgktp@users.noreply.github.com>
2023-07-07 11:58:14 -0400

    Fix bad indentation

--- demo.py
+++ demo.py
@@ -1,11 +1,12 @@
-    from nemoguardrails.rails import LLMRails, RailsConfig
-    def demo():
-        config = RailsConfig.from_path("./config")
-        rails = LLMRails(config)
-        response = rails.generate(messages=[{
-            "role": "user",
-            "content": input("Prompt: ")
-        }])
-        print(response)
-    if __name__ == "__main__":
-        demo()
+from nemoguardrails.rails import LLMRails, RailsConfig
+
+def demo():
+    config = RailsConfig.from_path("./config")
+    rails = LLMRails(config)
+    response = rails.generate(messages=[{
+        "role": "user",
+        "content": input("Prompt: ")
+    }])
+    print(response)
+if __name__ == "__main__":
+    demo()
